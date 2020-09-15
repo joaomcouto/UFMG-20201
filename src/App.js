c0e52483817921d69a65c4a480235b358bc13356
Marco Túlio <marcotmotta@gmail.com>
2020-09-15 15:17:31 -0300

    Adicionar biblioteca material-ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 import './App.css';
 
 function App() {
+
+  const [page, setPage] = React.useState("");
+
   return (
     <div className="App">
       <h1>It's Servi</h1>
       <p>...but it does nothing yet.</p>
+      {/* <input val=""></input> */}
+      <Button variant="contained" onClick={() => setPage('Arroz')}>Submit</Button>
     </div>
   );
 }
