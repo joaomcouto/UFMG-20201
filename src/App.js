ee0a6fc0cff6c2f02259b147032648b6c97da96d
Marco Túlio <marcotmotta@gmail.com>
2020-09-14 21:03:36 -0300

    Importação de fontes customizadas

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Comfortaa" />
-      <h1>I'ts Servi</h1>
+      <h1>It's Servi</h1>
       <p>...but it does nothing yet.</p>
     </div>
   );
