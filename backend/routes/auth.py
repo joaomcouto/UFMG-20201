7108149460354ce0343828871895ea5d64822807
Felipe Cadar Chamone <cadar@dcc.ufmg.br>
2020-09-17 20:35:11 -0300

    init auth

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,7 +1,13 @@
 from flask import Blueprint
+from modules.db import get_db
 
 auth_module = Blueprint("auth_module", __name__)
+db = get_db()
 
 @auth_module.route("/")
 def status():
     return 'Hello, World! From auth blueprint', 200
+
+@auth_module.route("/register",  methods=['POST'])
+def register():
+    print(request)
