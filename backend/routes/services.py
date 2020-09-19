06b2f51868faa79642fb1b4eb5b0c2ff492854fd
joaomcouto <joaom.couto55@gmail.com>
2020-09-18 21:36:41 -0300

    Added service query by name (GET)

--- backend/routes/services.py
+++ backend/routes/services.py
@@ -17,3 +17,11 @@ def get_services():
         return json.dumps(services, default=json_util.default)
     except Exception as err:
         logging.error(err)
+
+@services_module.route("/<nome_do_servico>", methods=['GET'])
+def getService(nome_do_servico):
+    try:
+        result = db["services"].find_one({'nome':nome_do_servico})
+        return json.dumps(result, default=json_util.default)
+    except Exception as err:
+        logging.error(err)
