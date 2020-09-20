4ffeed48677f129b2c492c7614318e4ea9852f91
Bruno Varella Peixoto <varellap@gmail.com>
2020-09-20 12:45:36 -0300

    adicionada rota de cadastro de pedido

--- backend/routes/orders.py
+++ backend/routes/orders.py
@@ -1,32 +1,16 @@
-
 import json
 import logging
 
 from bson import json_util
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from modules.db import get_db
 from bson.objectid import ObjectId
 
-from backend.modules.utils import JSONEncoder
-
-
+from backend.modules.db import orderModel
 
 orders_module = Blueprint("orders_module", __name__)
 db = get_db()
 
-# @orders_module.route("/user_id/<id_do_user>", methods=['GET'])
-# def getOrder(id_do_user):
-#     try:
-#         results = db["orders"].find({'user_id':ObjectId(id_do_user)})
-#         json_results = []
-#         for result in results:
-#             json_results.append(result)
-#         #result = db["orders"].find()
-#         #return JSONEncoder().encode(result)
-#         return json.dumps(json_results, default=json_util.default)
-#
-#     except Exception as err:
-#         logging.error(err)
 
 @orders_module.route("/", methods=['GET'])
 def getOrder():
@@ -49,15 +33,20 @@ def getOrder():
         logging.error(err)
 
 
-# @orders_module.route("/", methods=['GET'])
-# def getOrders():
-#     try:
-#         results = db["orders"].find()
-#         json_results = []
-#         for result in results:
-#             json_results.append(result)
-#         return json.dumps(json_results, default=json_util.default)
-#
-#
-#     except Exception as err:
-#         logging.error(err)
+@orders_module.route("/register", methods=['POST'])
+def register():
+    order_data = request.get_json()
+    try:
+        order = orderModel(**order_data)
+        try:
+            db["orders"].insert_one(order)
+        except Exception as err:
+            details = err._OperationFailure__details
+            details["errmsg"] = list(details["keyValue"].keys())[0] + " já está em uso"
+            return jsonify(details), 400
+
+        resp = jsonify(order)
+
+        return resp, 200
+    except Exception as err:
+        return jsonify({"code": 1, "errmsg": err.args[0]}), 400
