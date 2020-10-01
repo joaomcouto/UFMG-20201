3784f4cedebebcc5121dd228963f84eb50b93cee
joaomcouto <joaom.couto55@gmail.com>
2020-10-01 11:57:09 -0300

    Commit #2 - Restructured main package, added project files

---
+++ src/money/Dollar.java
@@ -0,0 +1,17 @@
+package money;
+
+class Dollar extends Money {
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+    public Dollar times(int multiplier) {
+        return new Dollar (this.amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object object){
+        Dollar dollar = (Dollar) object;
+        return this.amount == dollar.amount;
+    }
+}
---
+++ src/money/Money.java
@@ -0,0 +1,10 @@
+package money;
+
+class Money {
+    protected int amount;
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return this.amount == money.amount;
+    }
+}
