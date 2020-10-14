433593c5859d21a1dbb52282f0fb82de6dcd2964
joaomcouto <joaom.couto55@gmail.com>
2020-10-14 17:33:54 -0300

    Commit 4

--- Refactoring_Lab/src/Customer.java
+++ Refactoring_Lab/src/Customer.java
@@ -28,10 +28,8 @@ public class Customer {
         
        
         //determine amounts for each line
-        double thisAmount = amountFor(each);
+        double thisAmount = each.getCharge();
         
-
-
         // add frequent renter points
         frequentRenterPoints ++;
         // add bonus for a two day new release rental
@@ -51,25 +49,7 @@ public class Customer {
      return result;
    }
   
-  private double amountFor(Rental aRental) {
-      double thisAmount = 0;
-      switch (aRental.getMovie().getPriceCode()) {
-         case Movie.REGULAR:
-            thisAmount += 2;
-            if (aRental.getDaysRented() > 2)
-               thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-            break;
-         case Movie.NEW_RELEASE:
-            thisAmount += aRental.getDaysRented() * 3;
-            break;
-         case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (aRental.getDaysRented() > 3)
-               thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-             break;
-      }
-      return thisAmount;
-   }
+
   
   
 }
