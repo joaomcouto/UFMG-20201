ba9ee4ed6a4eb1b4d1548003fc225152d7bfd95a
joaomcouto <joaom.couto55@gmail.com>
2020-10-14 19:58:28 -0300

    Commit 17

--- Refactoring_Lab/src/HtmlStatement.java
+++ Refactoring_Lab/src/HtmlStatement.java
@@ -3,22 +3,36 @@ import java.util.Enumeration;
 public class HtmlStatement extends Statement {
 	
    public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = "<H1>Rentals for <EM>" + aCustomer.getName() +
-      "</EM></H1><P>\n";
+      Enumeration rentals = aCustomer.getRentals(); 
+      String result = valueTop(aCustomer);
       while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
+         Rental each = (Rental) rentals.nextElement(); 
          //show figures for each rental
-         result += each.getMovie().getTitle()+ ": " +
-         String.valueOf(each.getCharge()) + "<BR>\n";
+         result += valueMid(each);
       }
       //add footer lines
-      result += "<P>You owe <EM>" +
-      String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
-      result += "On this rental you earned <EM>" + 
-      String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-      "</EM> frequent renter points<P>";
+      result += valueBottom(aCustomer);
       return result;
    }
+   
+	public String valueTop(Customer aCustomer){
+		return "<H1>Rentals for <EM>" + aCustomer.getName() + 
+										 "</EM></H1><P>\n";
+	}
+	
+	public String valueMid(Rental each){
+		return each.getMovie().getTitle()+ ": " +
+		       String.valueOf(each.getCharge()) + 
+		       							"<BR>\n";
+		
+	}
+	
+	public String valueBottom(Customer aCustomer){
+		return	"<P>You owe <EM>" + String.valueOf(aCustomer.getTotalCharge()) + 
+																  "</EM><P>\n" +
+			      							  "On this rental you earned <EM>" + 
+			      	  String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+			                                  "</EM> frequent renter points<P>";
+	}
 
 }
--- Refactoring_Lab/src/TextStatement.java
+++ Refactoring_Lab/src/TextStatement.java
@@ -3,22 +3,34 @@ import java.util.Enumeration;
 public class TextStatement extends Statement {
 	
 	public String value(Customer aCustomer) {
-	  Enumeration rentals = aCustomer.getRentals();
-	  String result = "Rental Record for " + aCustomer.getName() +
-	  "\n";
+	  Enumeration rentals = aCustomer.getRentals(); 
+	  String result = valueTop(aCustomer) ;		  
 	  while (rentals.hasMoreElements()) {
-	     Rental each = (Rental) rentals.nextElement();
+	     Rental each = (Rental) rentals.nextElement(); 
 	     //show figures for this rental
-	     result += "\t" + each.getMovie().getTitle()+ "\t" +
-	     String.valueOf(each.getCharge()) + "\n";
+	     result += valueMid(each);
 	  }
 	  //add footer lines
-	  result += "Amount owed is " +
-	  String.valueOf(aCustomer.getTotalCharge()) + "\n";
-	  result += "You earned " +
-	  String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-	  " frequent renter points";
+	  result += valueBottom(aCustomer);
 	  return result;
+	}
+	
+	public String valueTop(Customer aCustomer){
+		return "Rental Record for " + aCustomer.getName() + "\n";
+	}
+	
+	public String valueMid(Rental each){
+		return "\t" + each.getMovie().getTitle()+ "\t" +
+			    String.valueOf(each.getCharge()) + "\n";
+		
+	}
+	
+	public String valueBottom(Customer aCustomer){
+		return "Amount owed is " + String.valueOf(aCustomer.getTotalCharge()) + 
+																		 "\n" +
+																"You earned " +
+				     String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+				                                     " frequent renter points";	
 	}
 
 }
