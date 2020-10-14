bcefe3d63e3a78fe039a13798a7d33c74d7cac05
joaomcouto <joaom.couto55@gmail.com>
2020-10-14 18:14:07 -0300

    Commit 10

--- Refactoring_Lab/src/Rental.java
+++ Refactoring_Lab/src/Rental.java
@@ -19,11 +19,11 @@ public class Rental {
    public double getCharge(){
 	   return _movie.getCharge(_daysRented);
    }
-
+///
    public int getFrequentRenterPoints(){
-       // add bonus for a two day new release rental
-       if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-           getDaysRented() > 1) return 2;
-       else return 1;
-	   }
+	   return _movie.getFrequentRenterPoints(_daysRented);
+   }
+   
+///   
+
 }
